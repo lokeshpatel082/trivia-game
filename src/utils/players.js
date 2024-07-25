bad7069484b842672e7d8575d5d20b2fef515bb7
Lokesh Patel <lokeshpatel082@gmail.com>
2024-07-25 16:10:24 +0530

    complete game logic

---
+++ src/utils/players.js
@@ -0,0 +1,60 @@
+const players = [];
+const addPlayer = ({id, playerName, room}) => {
+  if(!playerName || !room){
+    return {
+      error: new Error("Please enter a player name and room !"),
+    };
+  }
+
+  playerName = playerName.trim().toLowerCase();
+  room = room.trim().toLowerCase();
+  
+  const existingPlayer = players.find((player) => {
+    return player.room === room && player.playerName === playerName;
+  });
+
+  if(existingPlayer){
+    return {
+      error: new Error("Player name is in the use !")
+    };
+  }
+
+  const newPlayer = {id, playerName, room};
+  players.push(newPlayer);
+
+  return {newPlayer};
+};
+
+// get a player by id
+const getPlayer = (id) => {
+  const player = players.find((player)=>player.id === id);
+  if(!player){
+    return {
+      error: new Error("Player not found!")
+    };
+  }
+  return {player};
+};
+
+// Get all the players in the room
+const getAllPlayers = (room) => {
+  return players.filter((player) => player.room === room);
+};
+
+// Remove a player by id
+const removePlayer = (id) => {
+  return players.find((player, index) => {
+    if (player.id === id) {
+      return players.splice(index, 1)[0];
+    }
+    return false;
+  });
+};
+
+// Export our helper methods
+module.exports = {
+  addPlayer,
+  getPlayer,
+  getAllPlayers,
+  removePlayer,
+};
